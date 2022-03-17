87de14c102b6df94dca28681f8454678f53a08b5
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-03-17 22:45:57 +0300

    Fix wrong package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tfreplknet',
-    version='2.0.0',
+    version='1.0.1',
     description='Keras (TensorFlow v2) reimplementation of RepLKNet model.',
     long_description=long_description,
     long_description_content_type="text/markdown",
