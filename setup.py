9f2e51f7fb4d8fafc9a3fa3d6eac6ee626eae21a
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-03-30 12:26:46 +0300

    Update version, drop local weights testing stub

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tfreplknet',
-    version='1.0.2',
+    version='1.1.0',
     description='Keras (TensorFlow v2) reimplementation of RepLKNet model.',
     long_description=long_description,
     long_description_content_type="text/markdown",
